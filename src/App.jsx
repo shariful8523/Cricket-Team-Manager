ffb365758de47d86e45425d8d5585d945a7c66f8
shariful8523 <mdsharifulislam8523@gmail.com>
2024-10-24 22:27:04 +0600

    added the Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navabr from './componets/Navbar/Navabr'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       
-      <h1 className='font-bold text-9xl'>Vite + React</h1>
+     <Navabr></Navabr>
+
      
     </>
   )
---
+++ src/componets/Navbar/Navabr.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Profile from '../../assets/logo.png'
+
+
+function Navabr(props) {
+    return (
+        <div className='flex justify-between py-7 mx-auto w-11/12'>
+            <div className='w-14'>
+            <img src={Profile} alt="" />
+            </div>
+
+
+            <div className=' font-medium text-[rgba(19,19,19,0.7)] ml-auto px-9 mt-2 flex gap-8'>
+                <p>Home</p>
+                <p>Fixture</p>
+                <p>Teams</p>
+                <p>Schedules</p>
+            </div>
+            <div>
+            <button className='  font-bold p-2 px-7 rounded-lg  border border-[rgba(59,44,44,0.1)] '>
+                    
+                    Coin <span className='px-2'  ><i class="fa-solid fa-sack-dollar"></i></span> </button>
+            </div>
+        </div>
+    );
+}
+
+export default Navabr;
